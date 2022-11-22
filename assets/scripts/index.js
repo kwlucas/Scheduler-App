e1238eff73e44fdcacd8d18b3c4b843594a99143
kwlucas <lucaswkyle@gmail.com>
2022-11-22 12:30:50 -0700

    Created update interval and function

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,6 +1,6 @@
-function getHour(timeId){
+function getHour(timeId) {
     let hour = Number(timeId.match(/(?<=time-)\d{1,2}(?=[ap])/)[0]);
-    if(hour != 12 && timeId.includes('p')){
+    if (hour != 12 && timeId.includes('p')) {
         hour += 12;
     }
     return hour;
@@ -9,14 +9,14 @@ function getHour(timeId){
 function checkTimeblock(blockEl) {
     const currentHour = moment().hours()
     const checkHour = getHour(blockEl.attr('id'));
-    
-    if(checkHour > currentHour){
+
+    if (checkHour > currentHour) {
         blockEl.addClass('future');
-    } else if(checkHour == currentHour){
+    } else if (checkHour == currentHour) {
         blockEl.addClass('present');
         blockEl.removeClass('future');
     }
-    else if(checkHour < currentHour){
+    else if (checkHour < currentHour) {
         blockEl.addClass('past');
         blockEl.removeClass('present');
         blockEl.removeClass('future');
@@ -25,13 +25,20 @@ function checkTimeblock(blockEl) {
 
 $('document').ready(function () {
     $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
-    
-    $('.saveBtn').on('click', function() {
+
+    $('.saveBtn').on('click', function () {
         localStorage.setItem($(this).parent().attr('id'), $(this).siblings('.description').val());
     });
 
     $('.time-block').each(function () {
         $(this).children('.description').val(localStorage.getItem($(this).attr('id')));
         checkTimeblock($(this));
-    })
+    });
+
+    function updateBlocks() {
+        $('.time-block').each(function () {
+            checkTimeblock($(this));
+        });
+    };
+    let updateInterval = setInterval(updateBlocks, 15000);
 })
